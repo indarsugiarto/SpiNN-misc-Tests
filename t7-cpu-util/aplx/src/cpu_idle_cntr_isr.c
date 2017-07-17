1678001f3bb1ddcce98c535c94b05d0d696e0f74
Levionus Verbionae Indiensis <indi@localhost.localdomain>
2017-07-17 23:35:53 +0100

    initial update with t7

---
+++ t7-cpu-util/aplx/src/cpu_idle_cntr_isr.c
@@ -0,0 +1,42 @@
+/* The idea for cpu utilization using slow timer:
+ * - Slow timer runs at about 32kHz. Whenever it fires, it detects
+ *   the sleep state of each cores.
+ * - After 100 times counting, it generates software interrupt.
+ * - This 100 limit equivalent to 100%. Hence, if a core counter has 
+ *   the value of 100, it means that core is 100% utilized during the
+ *   sampling period of 1000000/32000 us == 3.124ms
+ */
+#include <sark.h>
+#include "
+extern uchar running_cpu_idle_cntr[18]; // an array of 18-cores idle counters
+extern uchar stored_cpu_idle_cntr[18];
+extern uint idle_cntr_cntr; // counter of idle counter
+
+INT_HANDLER hSlowTimer (void)
+{
+  dma[DMA_GCTL] = 0x7FFFFFFF; // clear bit[31] of dma GCTL
+
+  if(++idle_cntr_cntr==100) {
+    // copy to stored_cpu_idle_cntr
+    sark_mem_cpy(stored_cpu_idle_cntr, running_cpu_idle_cntr, 18);
+    // clear running_cpu_idle_cntr and idle_cntr_cntr
+    for(int i=0; i<18; i++)
+      running_cpu_idle_cntr[i] = 0;
+    idle_cntr_cntr = 0;
+    // trigger software interrupt for the main profiler loop
+  }
+  else {
+    // detect idle state of all cores
+  }
+  vic[VIC_VADDR] = (uint) vic;			// Tell VIC we're done
+}
+
+/* hSoftInt() is triggered when idle_cntr_cntr in hSlowTimer()
+ * reaches 100. This signify ends of sampling period.
+ * profiler should response this to collect the cpu utilization
+ * value.
+ */
+INT_HANDLER hSoftInt (void)
+{
+
+}
